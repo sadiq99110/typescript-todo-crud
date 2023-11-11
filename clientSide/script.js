6c3a09399ae989f58c5db2b6f72ba0792c7377cf
sadiq99110 <sadiq99110@gmail.com>
2023-11-11 20:56:57 +0500

    Some edit functionality changes

--- clientSide/script.js
+++ clientSide/script.js
@@ -1,5 +1,7 @@
+ var id;
+ var response;
  // Fetch and display existing todo items on page load
- fetch('https://typecript-todo-api-f653ff8527ee.herokuapp.com/api/v1/posts')
+ fetch('http://localhost:3000/api/v1/posts')
  .then(response => response.json())
  .then(data => {
      if (data.length > 0) {
@@ -31,7 +33,7 @@ function addTodo() {
  };
 
  // Use AJAX to post data to the server
- fetch('https://typecript-todo-api-f653ff8527ee.herokuapp.com/api/v1/posts', {
+ fetch('http://localhost:3000/api/v1/posts', {
      method: 'POST',
      headers: {
          'Content-Type': 'application/json',
@@ -53,6 +55,9 @@ document.getElementById('published').checked = false;
  });
 }
 
+var addButton = document.getElementById('addTodoItems')
+var updateButton = document.getElementById('editTodo')
+
 function updateTodoList(todo) {
 // Update the list of todo items
 var todoList = document.getElementById('todoList');
@@ -60,6 +65,7 @@ var listItem = document.createElement('li');
 
 // Create separate elements for title and buttons
 var titleElement = document.createElement('span');
+response = titleElement
 titleElement.innerHTML = `Title: ${todo.title}<br>Description: ${todo.description}<br>Author: ${todo.author}<br>Published: ${todo.published}`;
 listItem.appendChild(titleElement);
 
@@ -67,8 +73,6 @@ listItem.appendChild(titleElement);
 // Add edit button
 var editButton = document.createElement('button');
 editButton.textContent = 'Edit';
-var addButton = document.getElementById('addTodoItems')
-var updateButton = document.getElementById('editTodo')
 
 editButton.onclick  = function (){
 document.getElementById('title').value = todo.title;
@@ -77,41 +81,14 @@ document.getElementById('author').value = todo.author;
 document.getElementById('published').checked = todo.published;
 addButton.style.display = 'none'
 updateButton.style.display = 'block'
+id = todo._id
 }
 
 updateButton.onclick = function () {
-var title = document.getElementById('title').value
-var description = document.getElementById('description').value
-var author = document.getElementById('author').value
-var published = document.getElementById('published').checked
-console.log("title", title, description, author, published);
-
-if(title != null || description != null || author != null || published != null){
- fetch(`https://typecript-todo-api-f653ff8527ee.herokuapp.com/api/v1/posts/${todo._id}`, {
-     method: 'PUT',
-     headers: {
-         'Content-Type': 'application/json',
-     },
-     body: JSON.stringify({ title: title, description:description, author:author, published:published }),
- })
- .then(response => response.json())
- .then(updatedTodo => {
-     titleElement.textContent = `Title: ${title}, Description: ${description}, Author: ${author}, Published: ${published}`;
-     document.getElementById('title').value = '';
-     document.getElementById('description').value = '';
-     document.getElementById('author').value = '';
-     document.getElementById('published').checked = false;
-     addButton.style.display = 'block'
-    updateButton.style.display = 'none'
- })
- .catch(error => {
-     console.error('Error editing todo:', error);
- });
-}else{
- alert("Something went wrong")
+    // Pass the todo object as a parameter
+    updateTodo(todo);
 }
 
-}
 
 listItem.appendChild(editButton);
 
@@ -120,7 +97,7 @@ var deleteButton = document.createElement('button');
 deleteButton.textContent = 'Delete';
 deleteButton.onclick = function () {
 // Call your delete API here with the todo item's ID
-fetch(`https://typecript-todo-api-f653ff8527ee.herokuapp.com/api/v1/posts/${todo._id}`, {
+fetch(`http://localhost:3000/api/v1/posts/${todo._id}`, {
  method: 'DELETE',
 })
 .then(() => {
@@ -133,4 +110,41 @@ fetch(`https://typecript-todo-api-f653ff8527ee.herokuapp.com/api/v1/posts/${todo
 listItem.appendChild(deleteButton);
 
 todoList.appendChild(listItem);
+}
+
+
+function updateTodo(todo) {
+    var title = document.getElementById('title').value;
+    var description = document.getElementById('description').value;
+    var author = document.getElementById('author').value;
+    var published = document.getElementById('published').checked;
+
+    console.log("title", title, description, author, published);
+
+    if (title != null || description != null || author != null || published != null) {
+        console.log("Id", id);
+        fetch(`http://localhost:3000/api/v1/posts/${id}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({ title: title, description: description, author: author, published: published }),
+        })
+            .then(response => response.json())
+            .then(updatedTodo => {
+                location.reload();
+                response.textContent = `Title: ${updatedTodo.title}, Description: ${updatedTodo.description}, Author: ${updatedTodo.author}, Published: ${updatedTodo.published}`;
+                document.getElementById('title').value = '';
+                document.getElementById('description').value = '';
+                document.getElementById('author').value = '';
+                document.getElementById('published').checked = false;
+                addButton.style.display = 'block';
+                updateButton.style.display = 'none';
+            })
+            .catch(error => {
+                console.error('Error editing todo:', error);
+            });
+    } else {
+        alert("Something went wrong");
+    }
 }
